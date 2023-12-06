69cecd5a814af6e4e0a90fe7485851b47e5f0786
thehack <thehack@naver.com>
2023-12-06 18:29:43 +0900

    정리

--- WebContent/websquare/_websquare_/externalJS/editor4.22.1/config_3.js
+++ WebContent/websquare/_websquare_/externalJS/editor4.22.1/config_3.js
@@ -36,7 +36,7 @@ CKEDITOR.editorConfig = function( config )
 	                                  	['Rule','SpecialChar','PageBreak','TextColor','BGColor','ShowBlocks'],
 	                                  	['Styles','Format','Font','FontSize']		// No comma for the last row.
 	                                  ] ;
-	config.font_names	 = '굴림;궁서;돋움;바탕;Arial;Comic Sans MS;Courier New;Tahoma;Times New Roman;Verdana' ; 
+	config.font_names	 = '맑은 고딕;Times New Roman' ; 
 	config.fullPage = false;   //true이면 html태그 까지 보임 false이면 에디터 부분 태그만 보임(html로 보기 시에)
 	config.enterMode = CKEDITOR.ENTER_BR; //엔터시 입력되는 태그 BR P DIV 세 종류가 있습니다.
 	config.fontSize_sizes = '8/8px;9/9px;10/10px;11/11px;12/12px;14/14px;16/16px;18/18px;20/20px;22/22px;24/24px;26/26px;28/28px;36/36px;48/48px;72/72px' ; //폰트 사이즈 설정입니다.
