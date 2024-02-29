b5371b3cc9dfa6dd3bb4a1bd689a648058139f0b
javakorea <thehack@naver.com>
2024-02-29 21:16:12 +0900

    1

--- WebContent/_wpack_/cm/xml/footer.js
+++
@@ -1,4 +0,0 @@
-/*amd /cm/xml/footer.xml 707 4f823e4d71e9b1a8a79c829b475443aa2d631ad7421c5d05dbde4410832d977c */
-define({declaration:{A:{version:'1.0',encoding:'UTF-8'}},E:[{T:1,N:'html',A:{xmlns:'http://www.w3.org/1999/xhtml','xmlns:ev':'http://www.w3.org/2001/xml-events','xmlns:w2':'http://www.inswave.com/websquare','xmlns:xf':'http://www.w3.org/2002/xforms'},E:[{T:1,N:'head',E:[{T:1,N:'w2:type',E:[{T:3,text:'COMPONENT'}]},{T:1,N:'w2:buildDate'},{T:1,N:'xf:model',E:[{T:1,N:'w2:dataCollection',A:{baseNode:'map'}},{T:1,N:'w2:workflowCollection'}]},{T:1,N:'script',A:{type:'text/javascript',lazy:'false'},E:[{T:4,cdata:function(scopeObj){with(scopeObj){scwin.onpageload = function () {
-};
-}}}]}]},{T:1,N:'body',A:{'ev:onpageload':'scwin.onpageload'},E:[{T:1,N:'xf:group',A:{id:'grp_noticeArea',class:'notice_area'}}]}]}]})
