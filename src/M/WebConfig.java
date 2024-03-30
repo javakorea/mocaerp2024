b06152885cea47e0923558775fbe80487f317987
javakorea <thehack@naver.com>
2024-03-30 23:03:06 +0900

    커넥션풀업그래드와쿼리실행시간표시

--- src/M/WebConfig.java
+++ src/M/WebConfig.java
@@ -6,17 +6,23 @@ import javax.sql.DataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 @Configuration
 @EnableTransactionManagement
+@PropertySource("classpath:/application.properties")
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
@@ -30,6 +36,16 @@ public class WebConfig implements WebMvcConfigurer {
     
     @Autowired
 	ApplicationContext applicationContext;
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.hikari")
+    public HikariConfig hikariConfig() {
+        return new HikariConfig();
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        return new HikariDataSource(hikariConfig());
+    }
     @Bean
     public  SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
           final SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
