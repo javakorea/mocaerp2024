a5e24692e11d614cedab2a26f46cc5e55eb0c60b
javakorea <thehack@naver.com>
2024-05-05 12:58:53 +0900

    1

--- WebContent/cm/util.js
+++ WebContent/cm/util.js
@@ -376,7 +376,6 @@ function go_mobile (_objStr) {
 };
 function appToWeb(data){
 	if(data){
-		alert(data);
 		let arr = data.replace(/\s/g,'').replace(/\{|\}/g,'').split(',');
 		let userObj = {};
 		for(let i=0; i < arr.length; i++){
